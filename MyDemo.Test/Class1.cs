79bba584dc254f83ebf57b6ea79d26e8b81772ef
Harry Flanagan <S00189591@mail.itsligo.ie>
2020-03-02 17:47:09 +0000

    Add project files.

---
+++ MyDemo.Test/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MyDemo;
+
+
+namespace MyDemo.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public static void  t1()
+        {
+            //Arrange
+            MyDemo.Program p1 = new Program();
+
+            //Act
+            int actualresult = p1.Add(5, 10);
+
+            //Assert
+            Assert.That(actualresult, Is.EqualTo(15));
+
+        }
+        [Test]
+        public static void t2()
+        {
+            //Arrange
+            MyDemo.Program p1 = new Program();
+
+            //Act
+            int actualresult = p1.Add(5, 10);
+
+            //Assert
+            Assert.That(actualresult, Is.EqualTo(25));
+
+        }
+    }
+}
---
+++ MyDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Add(int a, int b)
+        {
+            int actualResult = a + b;
+            return actualResult;
+        }
+    }
+}
